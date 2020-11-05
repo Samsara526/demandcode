23e83e2ad9b1219bb3d0f2f1f900f3e28c89aa6d
Samsara <ruanduo2012@gmail.com>
2020-11-05 16:20:45 +0800

    添加了前端页面和控制器

---
+++ src/main/java/com/alandegtr/demandcode/web/HyperlinkController.java
@@ -0,0 +1,22 @@
+package com.alandegtr.demandcode.web;
+
+import com.alandegtr.demandcode.dao.HyperlinkRepository;
+import com.alandegtr.demandcode.po.Hyperlink;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HyperlinkController {
+
+    private final HyperlinkRepository hyperlinkRepository;
+
+    public HyperlinkController(HyperlinkRepository hyperlinkRepository) {
+        this.hyperlinkRepository = hyperlinkRepository;
+    }
+
+    @PostMapping(value = "/search")
+    public Hyperlink search(String demandCode) {
+        return hyperlinkRepository.findByDemandCode(demandCode);
+    }
+
+}
